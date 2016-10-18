f472c2798f6bc5bf0abce631eec7ec768ede451e
Samuel Monroe <spat.monroe@gmail.com>
2016-10-18 10:13:10 +0200

    Socket functions

---
+++ socket.c
@@ -0,0 +1,57 @@
+//
+// Created by Samuel Monroe on 5/10/16.
+//
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+int create_socket(struct sockaddr_in6 *source_addr, int src_port, struct sockaddr_in6 *dest_addr, int dst_port){
+    int sockfd = 0;
+
+    if((sockfd = socket(PF_INET6, SOCK_DGRAM, 0)) == -1){
+        fprintf(stderr, "Socket error : %s\n", gai_strerror(errno));
+        return -1;
+    };
+
+    if(src_port == -1){
+        // CLIENT
+        dest_addr->sin6_port = htons(dst_port);
+        if(connect(sockfd, (struct sockaddr*) dest_addr, sizeof(struct sockaddr_in6)) != 0){
+            fprintf(stderr, "Client connection error : %s\n", gai_strerror(errno));
+            return -1;
+        } else {
+            return sockfd;
+        }
+    } else {
+        // SERVER
+        source_addr->sin6_port = htons(src_port);
+        if(bind(sockfd, (struct sockaddr*) source_addr, sizeof(struct sockaddr_in6)) != 0){
+            fprintf(stderr, "Server socket error : %s\n", gai_strerror(errno));
+            return -1;
+        } else {
+            return sockfd;
+        }
+    }
+}
+
+const char * real_address(const char *address, struct sockaddr_in6 *rval){
+    struct addrinfo hints, *res;
+    const char * error;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_protocol = IPPROTO_UDP;
+    if(getaddrinfo(address, NULL, &hints, &res) == 0){
+        memcpy(rval, (struct sockaddr_in6 *) res->ai_addr, sizeof(*rval));
+        freeaddrinfo(res);
+        return NULL;
+    } else {
+        error = gai_strerror(errno);
+        freeaddrinfo(res);
+        return error;
+    };
+}
---
+++ socket.h
@@ -0,0 +1,34 @@
+#ifndef __SOCKET_H_
+#define __SOCKET_H_
+
+#include <netinet/in.h> /* * sockaddr_in6 */
+#include <sys/types.h> /* sockaddr_in6 */
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <errno.h>
+
+/* Creates a socket and initialize it
+ * @source_addr: if !NULL, the source address that should be bound to this socket
+ * @src_port: if >0, the port on which the socket is listening
+ * @dest_addr: if !NULL, the destination address to which the socket should send data
+ * @dst_port: if >0, the destination port to which the socket should be connected
+ * @return: a file descriptor number representing the socket,
+ *         or -1 in case of error (explanation will be printed on stderr)
+ */
+int create_socket(struct sockaddr_in6 *source_addr,
+                  int src_port,
+                  struct sockaddr_in6 *dest_addr,
+                  int dst_port);
+
+/* Resolve the resource name to an usable IPv6 address
+ * @address: The name to resolve
+ * @rval: Where the resulting IPv6 address descriptor should be stored
+ * @return: NULL if it succeeded, or a pointer towards
+ *          a string describing the error if any.
+ *          (const char* means the caller cannot modify or free the return value,
+ *           so do not use malloc!)
+ */
+const char * real_address(const char *address, struct sockaddr_in6 *rval);
+
+#endif
