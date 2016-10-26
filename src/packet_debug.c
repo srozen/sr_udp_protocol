4fe4a17c2f0c8e04934607435087ce90bca9f19f
Willfrit <remy-voet@outlook.fr>
2016-10-26 16:24:18 +0200

    remove payload in debug

--- src/packet_debug.c
+++ src/packet_debug.c
@@ -7,8 +7,6 @@ void pkt_debug(pkt_t * pkt){
     fprintf(stderr, " - Seqno : %d", pkt_get_seqnum(pkt));
     fprintf(stderr, " - Timestamp : %d\n", pkt_get_timestamp(pkt));
     fprintf(stderr, "Length is : %d\n", pkt_get_length(pkt));
-    const char * bf = pkt_get_payload(pkt);
-    fprintf(stderr, "Payload is : %s", bf);
     fprintf(stderr, "CRC is : %d\n", pkt_get_crc(pkt));
     fprintf(stderr, "###################################\n");
 }
